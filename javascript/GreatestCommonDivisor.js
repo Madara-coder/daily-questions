2e77bae6d6587dac92edccc8c468c49a8a02b1bd
Debrath Sharma <debrath.sharma@hazesoft.co>
2022-12-21 22:00:02 +0545

    completed the GCD question in php

---
+++ javascript/GreatestCommonDivisor.js
@@ -0,0 +1,2 @@
+// 1. Write a program to find the GCD (Greatest Common Divisor) of the given two numbers.
+
---
+++ python/GreatestCommonDivisor.py
@@ -0,0 +1,1 @@
+# 1. Write a program to find the GCD (Greatest Common Divisor) of the given two numbers.
