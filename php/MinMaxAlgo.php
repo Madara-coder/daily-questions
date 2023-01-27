d75aacbb0419fd79b156a9085a99b496a19411c2
Debrath Sharma <debrath.sharma@hazesoft.co>
2023-01-27 21:13:14 +0545

    MinMaxAlgo made easy in php

--- php/MinMaxAlgo.php
+++ php/MinMaxAlgo.php
@@ -1,12 +1,33 @@
 <?php
 
+// Divide and Conquer Algorithm.
+// Min-Max Algorithm.
 class MinMaxAlgo
 {
-    public function minMaxAlgo(): void
+    public function getMax(array $array): int
     {
+        $n = count($array);
+        $max = $array[0];
+        for ($i = 1; $i < $n; $i++) {
+            if ($max < $array[$i]) {
+                $max = $array[$i];
+            }
+        }
+        return $max;
+    }
 
+    public function getMin(array $array): int
+    {
+        $n = count($array);
+        $min = $array[0];
+        for ($i = 1; $i < $n; $i++) {
+            if ($min > $array[$i]) {
+                $min = $array[$i];
+            }
+        }
+        return $min;
     }
 }
 
 $object = new MinMaxAlgo;
-$object->minMaxAlgo();
+echo "The max number is: {$object->getMax([1,2,4,22,3,78,54,23])} and the min number is: {$object->getMin([1,2,4,22,3,78,54,23])}";
