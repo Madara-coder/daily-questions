188fb184675f33115964e93df3a05ec70135d1ba
Debrath Sharma <debrath.sharma@hazesoft.co>
2023-02-06 13:05:16 +0545

    stock buy and sell incomplete

---
+++ php/StockBuySell.php
@@ -0,0 +1,20 @@
+<?php
+
+class StockBuySell
+{
+    public function buySell(array $prices)
+    {
+        $maxProfit = 0;
+        $minSoFar = $prices[0];
+        // return min($prices);
+        // for ($i = 0; $i < count($prices); $i++) {
+        //     $minSoFar = min($prices);
+        //     $profit = $prices[i] - $minSoFar;
+        //     $maxProfit = max($prices);
+        // }
+    }
+    return $maxProfit;
+}
+
+$object = new StockBuySell;
+echo "The maximum profit from the given prices is: {$object->buySell([11,23,4,12,1,2])}";
